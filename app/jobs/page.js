1f1dba185850b4de9e5470d8f405161f018ef7cf
samuel-ponraj <samuelponraj.official@gmail.com>
2025-11-26 08:21:39 +0530

    updated

--- app/jobs/page.js
+++ app/jobs/page.js
@@ -1,15 +1,35 @@
+// 'use client'
+// import { useSearchParams } from 'next/navigation'
+// import JobListPage from "../../components/jobs/joblist/JobListPage";
+
+// export default function Page() {
+
+//   const searchParams = useSearchParams();
+//   const keyword = searchParams.get("search") || "";
+  
+//   return (
+//     <div>
+//       <JobListPage search ={keyword} /> 
+//     </div>
+//   );
+// }
+
 'use client'
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from "react";
 import JobListPage from "../../components/jobs/joblist/JobListPage";
 
-export default function Page() {
-
+function PageContent() {
   const searchParams = useSearchParams();
   const keyword = searchParams.get("search") || "";
-  
+  return <JobListPage search={keyword} />;
+}
+
+export default function Page() {
   return (
-    <div>
-      <JobListPage search ={keyword} /> 
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <PageContent />
+    </Suspense>
   );
 }
+
